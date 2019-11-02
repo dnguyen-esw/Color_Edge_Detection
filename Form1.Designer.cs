188176a2629dcfec0e7a8143fd0fe1b3ac2a18cb
hoangduc104 <48839695+hoangduc104@users.noreply.github.com>
2019-11-03 01:47:52 +0700

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace XLA_Project08
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox_hinhgoc = new System.Windows.Forms.PictureBox();
+            this.label_hinhgoc = new System.Windows.Forms.Label();
+            this.label_edge = new System.Windows.Forms.Label();
+            this.pictureBox_edge = new System.Windows.Forms.PictureBox();
+            this.button_sobel = new System.Windows.Forms.Button();
+            this.button_prewitt = new System.Windows.Forms.Button();
+            this.label_nguong = new System.Windows.Forms.Label();
+            this.textBox_nguong = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_hinhgoc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_edge)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_hinhgoc
+            // 
+            this.pictureBox_hinhgoc.Location = new System.Drawing.Point(12, 42);
+            this.pictureBox_hinhgoc.Name = "pictureBox_hinhgoc";
+            this.pictureBox_hinhgoc.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox_hinhgoc.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_hinhgoc.TabIndex = 0;
+            this.pictureBox_hinhgoc.TabStop = false;
+            // 
+            // label_hinhgoc
+            // 
+            this.label_hinhgoc.AutoSize = true;
+            this.label_hinhgoc.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_hinhgoc.Location = new System.Drawing.Point(201, 18);
+            this.label_hinhgoc.Name = "label_hinhgoc";
+            this.label_hinhgoc.Size = new System.Drawing.Size(124, 21);
+            this.label_hinhgoc.TabIndex = 1;
+            this.label_hinhgoc.Text = "Hình RGB gốc";
+            // 
+            // label_edge
+            // 
+            this.label_edge.AutoSize = true;
+            this.label_edge.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_edge.Location = new System.Drawing.Point(979, 18);
+            this.label_edge.Name = "label_edge";
+            this.label_edge.Size = new System.Drawing.Size(187, 21);
+            this.label_edge.TabIndex = 3;
+            this.label_edge.Text = "Hình đã nhận dạng biên";
+            // 
+            // pictureBox_edge
+            // 
+            this.pictureBox_edge.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox_edge.Location = new System.Drawing.Point(820, 42);
+            this.pictureBox_edge.Name = "pictureBox_edge";
+            this.pictureBox_edge.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox_edge.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_edge.TabIndex = 2;
+            this.pictureBox_edge.TabStop = false;
+            // 
+            // button_sobel
+            // 
+            this.button_sobel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button_sobel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_sobel.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_sobel.ForeColor = System.Drawing.Color.DarkRed;
+            this.button_sobel.Location = new System.Drawing.Point(603, 193);
+            this.button_sobel.Name = "button_sobel";
+            this.button_sobel.Size = new System.Drawing.Size(138, 85);
+            this.button_sobel.TabIndex = 5;
+            this.button_sobel.Text = "Sobel\'s mask";
+            this.button_sobel.UseVisualStyleBackColor = false;
+            this.button_sobel.Click += new System.EventHandler(this.button_sobel_Click);
+            // 
+            // button_prewitt
+            // 
+            this.button_prewitt.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button_prewitt.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_prewitt.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_prewitt.ForeColor = System.Drawing.Color.DarkRed;
+            this.button_prewitt.Location = new System.Drawing.Point(603, 284);
+            this.button_prewitt.Name = "button_prewitt";
+            this.button_prewitt.Size = new System.Drawing.Size(138, 85);
+            this.button_prewitt.TabIndex = 6;
+            this.button_prewitt.Text = "Prewitt\'s mask";
+            this.button_prewitt.UseVisualStyleBackColor = false;
+            this.button_prewitt.Click += new System.EventHandler(this.button_prewitt_Click);
+            // 
+            // label_nguong
+            // 
+            this.label_nguong.AutoSize = true;
+            this.label_nguong.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_nguong.Location = new System.Drawing.Point(581, 148);
+            this.label_nguong.Name = "label_nguong";
+            this.label_nguong.Size = new System.Drawing.Size(74, 21);
+            this.label_nguong.TabIndex = 7;
+            this.label_nguong.Text = "Ngưỡng:";
+            // 
+            // textBox_nguong
+            // 
+            this.textBox_nguong.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_nguong.Location = new System.Drawing.Point(661, 145);
+            this.textBox_nguong.Name = "textBox_nguong";
+            this.textBox_nguong.Size = new System.Drawing.Size(99, 29);
+            this.textBox_nguong.TabIndex = 4;
+            this.textBox_nguong.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1492, 683);
+            this.Controls.Add(this.label_nguong);
+            this.Controls.Add(this.button_prewitt);
+            this.Controls.Add(this.button_sobel);
+            this.Controls.Add(this.textBox_nguong);
+            this.Controls.Add(this.label_edge);
+            this.Controls.Add(this.pictureBox_edge);
+            this.Controls.Add(this.label_hinhgoc);
+            this.Controls.Add(this.pictureBox_hinhgoc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_hinhgoc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_edge)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_hinhgoc;
+        private System.Windows.Forms.Label label_hinhgoc;
+        private System.Windows.Forms.Label label_edge;
+        private System.Windows.Forms.PictureBox pictureBox_edge;
+        private System.Windows.Forms.Button button_sobel;
+        private System.Windows.Forms.Button button_prewitt;
+        private System.Windows.Forms.Label label_nguong;
+        private System.Windows.Forms.TextBox textBox_nguong;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XLA_Project08
+{
+    public partial class Form1 : Form
+    {
+        Bitmap hinhgoc;
+        public Form1()
+        {
+            InitializeComponent();
+            hinhgoc = new Bitmap(@"C:\Users\hoang\OneDrive\Máy tính\Image Processing\C#\lena_color.gif");
+            pictureBox_hinhgoc.Image = hinhgoc;
+            textBox_nguong.Text = "130";//ngưỡng mặc định
+        }
+        public Bitmap Edge_Detect_Sobel(Bitmap HinhGoc, int D0)
+        {
+            //sobel gx
+            int[,] mask_x = new int[3, 3];
+            mask_x[0, 0] = -1; mask_x[1, 0] = -2; mask_x[2, 0] = -1;
+            mask_x[0, 1] = 0; mask_x[1, 1] = 0; mask_x[2, 1] = 0;
+            mask_x[0, 2] = 1; mask_x[1, 2] = 2; mask_x[2, 2] = 1;
+            //sobel gy
+            int[,] mask_y = new int[3, 3];
+            mask_y[0, 0] = -1; mask_y[1, 0] = 0; mask_y[2, 0] = 1;
+            mask_y[0, 1] = -2; mask_y[1, 1] = 0; mask_y[2, 1] = 2;
+            mask_y[0, 2] = -1; mask_y[1, 2] = 0; mask_y[2, 2] = 1;
+
+            Bitmap edge = new Bitmap(HinhGoc.Width, HinhGoc.Height);
+
+            for (int x = 1; x < HinhGoc.Width - 1; x++)
+                for (int y = 1; y < HinhGoc.Height - 1; y++)
+                {
+                    double gxR = 0, gyR = 0;
+                    double gxG = 0, gyG = 0;
+                    double gxB = 0, gyB = 0;
+
+                    double gxx = 0;
+                    double gyy = 0;
+                    double gxy = 0;
+                    for (int i = x - 1; i <= x + 1; i++)
+                        for (int j = y - 1; j <= y + 1; j++)
+                        {
+                            Color color = HinhGoc.GetPixel(i, j);
+                            byte R = color.R;
+                            byte G = color.G;
+                            byte B = color.B;
+
+                            gxR += R * mask_x[i - (x - 1), j - (y - 1)];//tích chập ma trận mask_x và ma trận 3x3 giá trị các điểm ảnh lân cận f(x,y)
+                            gyR += R * mask_y[i - (x - 1), j - (y - 1)];//tích chập ma trận mask_y và ma trận 3x3 giá trị các điểm ảnh lân cận f(x,y)
+                    
+
+                            gxG += G * mask_x[i - (x - 1), j - (y - 1)];//tích chập ma trận mask_x và ma trận 3x3 giá trị các điểm ảnh lân cận f(x,y)
+                            gyG += G * mask_y[i - (x - 1), j - (y - 1)];//tích chập ma trận mask_y và ma trận 3x3 giá trị các điểm ảnh lân cận f(x,y)
+
+
+                            gxB += B * mask_x[i - (x - 1), j - (y - 1)];//tích chập ma trận mask_x và ma trận 3x3 giá trị các điểm ảnh lân cận f(x,y)
+                            gyB += B * mask_y[i - (x - 1), j - (y - 1)];//tích chập ma trận mask_y và ma trận 3x3 giá trị các điểm ảnh lân cận f(x,y)
+                        }
+                    gxx = Math.Pow(gxR, 2) + Math.Pow(gxG, 2) + Math.Pow(gxB, 2);
+                    gyy = Math.Pow(gyR, 2) + Math.Pow(gyG, 2) + Math.Pow(gyB, 2);
+                    gxy = gxR * gyR + gxG * gyG + gxB * gyB;
+                    //double theta=2*gxy/(gxx-gyy)
+                    double theta = 0.5*Math.Atan2(2 * gxy, gxx - gyy);
+
+                    //theta = Math.Abs(theta)+0.5*Math.PI;
+                    double z = theta * 180 / Math.PI;
+                    Console.WriteLine(z);
+                    //if (theta < 0) theta = theta + 0.5 * Math.PI;
+                    
+                    double F0 = Math.Sqrt(((gxx+gyy)+(gxx-gyy)*Math.Cos(2*theta)+2*gxy*Math.Sin(2*theta))/2);
+                    if (F0 <= D0)
+                        edge.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                    else
+                        edge.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                }
+
+            return edge;
+        }
+
+        public Bitmap Edge_Detect_Sobel_v2(Bitmap HinhGoc, int D0)
+        {
+            //sobel gx
+            int[,] dx = new int[3, 3];
+            dx[0, 0] = -1; dx[1, 0] = -2; dx[2, 0] = -1;
+            dx[0, 1] = 0; dx[1, 1] = 0; dx[2, 1] = 0;
+            dx[0, 2] = 1; dx[1, 2] = 2; dx[2, 2] = 1;
+            //sobel gy
+            int[,] dy = new int[3, 3];
+            dy[0, 0] = -1; dy[1, 0] = 0; dy[2, 0] = 1;
+            dy[0, 1] = -2; dy[1, 1] = 0; dy[2, 1] = 2;
+            dy[0, 2] = -1; dy[1, 2] = 0; dy[2, 2] = 1;
+
+            Bitmap edge = new Bitmap(HinhGoc.Width, HinhGoc.Height);
+
+            for (int x = 1; x < HinhGoc.Width - 1; x++)
+                for (int y = 1; y < HinhGoc.Height - 1; y++)
+                {
+                    double[] gx = new double[3];
+                    double[] gy = new double[3];
+
+                    double gxx = 0;
+                    double gyy = 0;
+                    double gxy = 0;
+                    for (int i = x - 1; i <= x + 1; i++)
+                        for (int j = y - 1; j <= y + 1; j++)
+                        {
+                            Color color = HinhGoc.GetPixel(i, j);
+                            byte R = color.R;//get red
+                            byte G = color.G;//get green
+                            byte B = color.B;//get blue
+
+                            gx[0] += R * dx[i - (x - 1), j - (y - 1)];//Red convolution  - X
+                            gy[0] += R * dy[i - (x - 1), j - (y - 1)];//Red convolution  - y
+                            
+                            gx[1] += G * dx[i - (x - 1), j - (y - 1)];//Green convolution - x
+                            gy[1] += G * dy[i - (x - 1), j - (y - 1)];//Green convolution - y
+                            
+                            gx[2] += B * dx[i - (x - 1), j - (y - 1)];//Blue convolution - x
+                            gy[2] += B * dy[i - (x - 1), j - (y - 1)];//Blue convolution - y
+                        }
+                    gxx = Math.Pow(gx[0], 2) + Math.Pow(gx[1], 2) + Math.Pow(gx[2], 2); //dot product
+                    gyy = Math.Pow(gy[0], 2) + Math.Pow(gy[1], 2) + Math.Pow(gy[2], 2); 
+                    gxy = gx[0] * gy[0] + gx[1] * gy[1] + gx[2] * gy[2];
+                    
+                    double theta = 0.5 * Math.Atan(2 * gxy/ (gxx - gyy)); //direction of maximum rate of change of pixxel c(x, y)
+                   
+                    double F0 = Math.Sqrt(((gxx + gyy) + (gxx - gyy) * Math.Cos(2 * theta) + 2 * gxy * Math.Sin(2 * theta)) *0.5); //the value of the rate of change at(x, y)
+                    if (F0 <= D0)
+                        edge.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                    else
+                        edge.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                }
+
+            return edge;
+        }
+
+
+        public Bitmap Edge_Detect_Prewittl(Bitmap HinhGoc, int D0)
+        {
+            //prewitt gx
+            int[,] mask_x = new int[3, 3];
+            mask_x[0, 0] = -1; mask_x[1, 0] = -1; mask_x[2, 0] = -1;
+            mask_x[0, 1] = 0; mask_x[1, 1] = 0; mask_x[2, 1] = 0;
+            mask_x[0, 2] = 1; mask_x[1, 2] = 1; mask_x[2, 2] = 1;
+            //prewitt gy
+            int[,] mask_y = new int[3, 3];
+            mask_y[0, 0] = -1; mask_y[1, 0] = 0; mask_y[2, 0] = 1;
+            mask_y[0, 1] = -1; mask_y[1, 1] = 0; mask_y[2, 1] = 1;
+            mask_y[0, 2] = -1; mask_y[1, 2] = 0; mask_y[2, 2] = 1;
+
+            Bitmap edge = new Bitmap(HinhGoc.Width, HinhGoc.Height);
+
+            for (int x = 1; x < HinhGoc.Width - 1; x++)
+                for (int y = 1; y < HinhGoc.Height - 1; y++)
+                {
+                    double gxR = 0, gyR = 0;
+                    double gxG = 0, gyG = 0;
+                    double gxB = 0, gyB = 0;
+
+                    double gxx = 0;
+                    double gyy = 0;
+                    double gxy = 0;
+                    for (int i = x - 1; i <= x + 1; i++)
+                        for (int j = y - 1; j <= y + 1; j++)
+                        {
+                            Color color = HinhGoc.GetPixel(i, j);
+                            byte R = color.R;
+                            byte G = color.G;
+                            byte B = color.B;
+
+                            gxR += R * mask_x[i - (x - 1), j - (y - 1)];//tích chập ma trận mask_x và ma trận 3x3 giá trị các điểm ảnh lân cận f(x,y)
+                            gyR += R * mask_y[i - (x - 1), j - (y - 1)];//tích chập ma trận mask_y và ma trận 3x3 giá trị các điểm ảnh lân cận f(x,y)
+
+
+                            gxG += G * mask_x[i - (x - 1), j - (y - 1)];//tích chập ma trận mask_x và ma trận 3x3 giá trị các điểm ảnh lân cận f(x,y)
+                            gyG += G * mask_y[i - (x - 1), j - (y - 1)];//tích chập ma trận mask_y và ma trận 3x3 giá trị các điểm ảnh lân cận f(x,y)
+
+
+                            gxB += B * mask_x[i - (x - 1), j - (y - 1)];//tích chập ma trận mask_x và ma trận 3x3 giá trị các điểm ảnh lân cận f(x,y)
+                            gyB += B * mask_y[i - (x - 1), j - (y - 1)];//tích chập ma trận mask_y và ma trận 3x3 giá trị các điểm ảnh lân cận f(x,y)
+                        }
+                    gxx = Math.Pow(Math.Abs(gxR), 2) + Math.Pow(Math.Abs(gxG), 2) + Math.Pow(Math.Abs(gxB), 2);
+                    gyy = Math.Pow(Math.Abs(gyR), 2) + Math.Pow(Math.Abs(gyG), 2) + Math.Pow(Math.Abs(gyB), 2);
+                    gxy = gxR * gyR + gxG * gyG + gxB * gyB;
+                    double theta = Math.Atan(2 * gxy/ (gxx - gyy)) / 2.0;
+                    double F0 = Math.Sqrt(((gxx + gyy) + (gxx - gyy) * Math.Cos(2 * theta) + 2 * gxy * Math.Sin(2 * theta)) / 2);
+
+                    if (F0 <= D0)
+                        edge.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                    else
+                        edge.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                }
+
+            return edge;
+        }
+        private void button_sobel_Click(object sender, EventArgs e)
+        {
+            pictureBox_edge.Image = Edge_Detect_Sobel_v2(hinhgoc, Convert.ToInt32(textBox_nguong.Text));
+        }
+
+        private void button_prewitt_Click(object sender, EventArgs e)
+        {
+
+            pictureBox_edge.Image = Edge_Detect_Prewittl(hinhgoc, Convert.ToInt32(textBox_nguong.Text));
+        }
+    }
+}
