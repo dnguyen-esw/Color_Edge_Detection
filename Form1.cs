758cd42483f85fad0b03397ae97a52f2048819a0
Nguyễn Hoàng Đức <48839695+ng-hoangduc@users.noreply.github.com>
2019-11-09 11:49:57 +0700

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -20,64 +20,6 @@ namespace XLA_Project08
             pictureBox_hinhgoc.Image = hinhgoc;
             textBox_nguong.Text = "130";//ngưỡng mặc định
         }
-        public Bitmap Edge_Detect_Sobel(Bitmap HinhGoc, int D0)
-        {
-            //sobel gx
-            int[,] mask_x = new int[3, 3];
-            mask_x[0, 0] = -1; mask_x[1, 0] = -2; mask_x[2, 0] = -1;
-            mask_x[0, 1] = 0; mask_x[1, 1] = 0; mask_x[2, 1] = 0;
-            mask_x[0, 2] = 1; mask_x[1, 2] = 2; mask_x[2, 2] = 1;
-            //sobel gy
-            int[,] mask_y = new int[3, 3];
-            mask_y[0, 0] = -1; mask_y[1, 0] = 0; mask_y[2, 0] = 1;
-            mask_y[0, 1] = -2; mask_y[1, 1] = 0; mask_y[2, 1] = 2;
-            mask_y[0, 2] = -1; mask_y[1, 2] = 0; mask_y[2, 2] = 1;
-
-            Bitmap edge = new Bitmap(HinhGoc.Width, HinhGoc.Height);
-
-            for (int x = 1; x < HinhGoc.Width - 1; x++)
-                for (int y = 1; y < HinhGoc.Height - 1; y++)
-                {
-                    double gxR = 0, gyR = 0;
-                    double gxG = 0, gyG = 0;
-                    double gxB = 0, gyB = 0;
-
-                    double gxx = 0;
-                    double gyy = 0;
-                    double gxy = 0;
-                    for (int i = x - 1; i <= x + 1; i++)
-                        for (int j = y - 1; j <= y + 1; j++)
-                        {
-                            Color color = HinhGoc.GetPixel(i, j);
-                            byte R = color.R;
-                            byte G = color.G;
-                            byte B = color.B;
-
-                            gxR += R * mask_x[i - (x - 1), j - (y - 1)];//tích chập ma trận mask_x và ma trận 3x3 giá trị các điểm ảnh lân cận f(x,y)
-                            gyR += R * mask_y[i - (x - 1), j - (y - 1)];//tích chập ma trận mask_y và ma trận 3x3 giá trị các điểm ảnh lân cận f(x,y)
-                    
-
-                            gxG += G * mask_x[i - (x - 1), j - (y - 1)];//tích chập ma trận mask_x và ma trận 3x3 giá trị các điểm ảnh lân cận f(x,y)
-                            gyG += G * mask_y[i - (x - 1), j - (y - 1)];//tích chập ma trận mask_y và ma trận 3x3 giá trị các điểm ảnh lân cận f(x,y)
-
-
-                            gxB += B * mask_x[i - (x - 1), j - (y - 1)];//tích chập ma trận mask_x và ma trận 3x3 giá trị các điểm ảnh lân cận f(x,y)
-                            gyB += B * mask_y[i - (x - 1), j - (y - 1)];//tích chập ma trận mask_y và ma trận 3x3 giá trị các điểm ảnh lân cận f(x,y)
-                        }
-                    gxx = Math.Pow(gxR, 2) + Math.Pow(gxG, 2) + Math.Pow(gxB, 2);
-                    gyy = Math.Pow(gyR, 2) + Math.Pow(gyG, 2) + Math.Pow(gyB, 2);
-                    gxy = gxR * gyR + gxG * gyG + gxB * gyB;
-                    double theta = 0.5*Math.Atan(2 * gxy/( gxx - gyy));
-                    
-                    double F0 = Math.Sqrt(((gxx+gyy)+(gxx-gyy)*Math.Cos(2*theta)+2*gxy*Math.Sin(2*theta))/2);
-                    if (F0 <= D0)
-                        edge.SetPixel(x, y, Color.FromArgb(0, 0, 0));
-                    else
-                        edge.SetPixel(x, y, Color.FromArgb(255, 255, 255));
-                }
-
-            return edge;
-        }
 
         public Bitmap Edge_Detect_Sobel_v2(Bitmap HinhGoc, int D0)//code rút gọn
         {
